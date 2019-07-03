3a06c57896f1445a45b283542f5ffa9d93ff1ae6
User <User@Samuel-PC>
2019-07-03 11:11:22 -0400

    Segundo ajuste

--- ProyectoFinal/src/Logica/LOL.java
+++ ProyectoFinal/src/Logica/LOL.java
@@ -1,5 +1,19 @@
 package Logica;
 
 public class LOL {
+ private String test;
 
+public LOL(String test) {
+	super();
+	this.test = test;
+}
+
+public String getTest() {
+	return test;
+}
+
+public void setTest(String test) {
+	this.test = test;
+}
+ 
 }

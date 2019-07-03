2519ee76daceb20e91be6936f58ad66b1d7b6e37
Angel Espinal <adespinal56@gmail.com>
2019-07-03 11:20:17 -0400

    rjhajvb

--- ProyectoFinal/src/Logica/LOL.java
+++ ProyectoFinal/src/Logica/LOL.java
@@ -2,6 +2,20 @@ package Logica;
 
 public class LOL {
  private String test;
+ private int xd;
+
+public int getXd() {
+	return xd;
+}
+
+public void setXd(int xd) {
+	this.xd = xd;
+}
+
+public LOL(int xd) {
+	super();
+	this.xd = xd;
+}
 
 public LOL(String test) {
 	super();

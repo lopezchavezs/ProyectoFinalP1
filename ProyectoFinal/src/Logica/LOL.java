0a5e1b1f401e08a70b6c3fa4ddc8fedd1f0a431e
User <User@Samuel-PC>
2019-07-22 11:23:29 -0400

    Nuevo Comienzo

--- ProyectoFinal/src/Logica/LOL.java
+++ ProyectoFinal/src/Logica/LOL.java
@@ -6,47 +6,4 @@ public class LOL {
  private String tuma;
  private int numero;
 
-public int getNumero() {
-	return numero;
-}
-
-public void setNumero(int numero) {
-	this.numero = numero;
-}
-
-public int getXd() {
-	return xd;
-}
-
-public String getTuma() {
-	return tuma;
-}
-
-public void setTuma(String tuma) {
-	this.tuma = tuma;
-}
-
-public void setXd(int xd) {
-	this.xd = xd;
-}
-
-public LOL(int xd) {
-	super();
-	this.xd = xd;
-}
-
-public LOL(String test) {
-	super();
-	this.test = test;
-}
-
-public String getTest() {
-	return test;
-}
-
-public void setTest(String test) {
-	this.test = test;
-}
-//SAmule vamo a buca cuero
- 
 }
